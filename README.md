2f58823870ae141f36df28aba79f20276f7e42f9
Robert McLeod <robert@datawallet.com>
2020-10-23 16:55:37 +0200

    Initial rough top level class definition

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Library to interact with ERC725 smart contracts
 
-**This package is currently empty, please don't use just yet**
+**This package is currently in early stages of development, please don't use just yet**
 
 ## Installation
 
@@ -42,3 +42,38 @@ universalprofile.getData('LSPXjsonURI')
 universalprofile.getData('LSP2Name')
 > 'My Cool name'
 ```
+
+
+### Example Schema
+
+```js
+let schema = [
+    {
+        "name": "LSP2Name",
+        "key": "0xf9e26448acc9f20625c059a95279675b8f58ba4f06d262f83a32b4dd35dee019",
+        "keyType": "Singleton",
+        "valueContent": "String",
+        "valueType": "string"
+    },
+    {
+        "name": "LSP2Links",
+        "key": "0xb95a64d66e66f5c0cd985e2c3cc93fbea7f9259eadbe81c3ab0ff4e68df564d6",
+        "keyType": "Singleton",
+        "valueContent": "URI",
+        "valueType": "string"
+    },
+    {
+        "name": "LSP2IssuedAssets[]",
+        "key": "0xb8c4a0b76ed8454e098b20a987a980e69abe3b1a88567ae5472af5f863f8c8f9",
+        "keyType": "Array",
+        "valueContent": "ArrayLength",
+        "valueType": "uint256",
+        "elementKey": "0xb8c4a0b76ed8454e098b20a987a980e6",
+        "elementKeyType": "ArrayElement",
+        "elementValueContent": "Address",
+        "elementValueType": "address"
+    }
+]
+```
+
+
--- src/index.js
+++ src/index.js
@@ -17,35 +17,108 @@
  * @date 2020
  */
 
-import { utils } from 'Web3'
+import * as web3utils from 'web3-utils'
 
 // TODO: First initial steps
-// 0. Get the basic library definitions from the ERC725 spec basics
-// import some type of ERC735 library standard
-
-// 1. Bring in the schema, addres, and provider
-
-// 2. attach schema
-
+// 0. Get the basic library definitions from the ERC725 spec basicc import some type of ERC735 library standard
+// 1. Bring in the schema, addres, and provider during instantiation
+// 2. Attach schema
 // 3. Attach web3 provider
 
 // NOTE: dont attach full web3 provider, just the provider object
-
+// NOTE: We want to avoid using whole web3 library
+// NOTE: Most likely create seperate source files for dataSrouce types based on provider
 
 export class ERC725 {
   constructor(schema, address, provider) {
-    super(props)
-    // we are adding the schema, the address and the provider(type?)
-    // the schema will need to be attached to this class
-
-    // Add more sophistiacted includes/checks
-    this.currentProvider = props.provider
-    this.contractAddress = props.address
+    super()
+    // TODO: Add more sophistiacted includes/checks
+    this.options = {
+      schema, // typeof Array
+      contractAddress: address,
+    }
+    this.currentProvider = provider // follows web3 'Contract' convention
+    // this.setProvider = this.setProvider.bind(this)
   }
 
-  get getData(key) {
+  // placeholder method
+  _setProvider() {
+
+  }
+  
+
+  _decodeDataByType(type, value) {
+    // TODO: add type checks
+    switch (type) {
+      case "String":
+        return value
+      case "Address":
+        break;
+      case "Keccak256":
+        break;
+      case "HashedAssetURI":
+        break;
+      case "JSONURI":
+        break;
+      case "URI":
+        break;
+      case "Markdown":
+        break;
+      case type.substr(0,2) === "0x":
+        break;
+    
+      default:
+        break;
+    }
+
+  }
+
+  _getDataFromSource() {
+    // querey the actual source
+    // We will need to check the currentProvider for which type it is...
+
+    if (currentSourceType === 'graphql') {
+      // query ERC725 subraph
+    } else {
+      // query web3 or ethereum rpc
+    }
+
+  }
+
+  _getKeyNameHash(name) {
+    // return the keccack265 hash of the string
+    return web3utils.keccak256(string)
+  }
+
+
+
+  getData(key) {
     // this needs to know the keyname hash... technically we don't need the string name
-    const keyName = utils.keccak256(string)
-    // check the schema for the type of name?
+    // TODO: check for firt 'bytes' to see of it's a hash?
+    let keyHash
+    if (key.substr(0,2) !== "0x") {
+      // Assumes no plain text names starting with 0x
+      keyhash = this._getKeyNameHash(key)
+    } else {
+      keyHash = key
+    }
+
+    // Get the correct schema key definition
+    let keyDefinition = null
+    for (let i = 0; i < this.options.schema.length; i++) {
+      const e = this.options.schema[i];
+      if (e === keyHash) {
+        keyDefinition = e
+        break
+      }
+    }
+
+    if (!keyDefinition) {
+      return Error('there is no key of this name defined')
+    }
+
+    // Now we must get the data
+    this._getDataFromSource(keyDefinition)
+
   }
 }
