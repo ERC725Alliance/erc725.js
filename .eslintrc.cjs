c1d63a9bcbde5395fd5ea3037387f3b44c4b78bb
Robert McLeod <robert@datawallet.com>
2020-11-25 14:31:03 +0100

    lint file update.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,43 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    'import/extensions': ['warn', 'always', { js: 'ignorePackages' }],
-    'no-multiple-empty-lines': ['error', { max: 2 }],
-    semi: ['error', 'never'],
-    'no-underscore-dangle': ['error', { allowAfterThis: true }],
-    indent: ['error', 4],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        mocha: true
+    },
+    extends: [
+        'airbnb-base'
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    rules: {
+        // Mocha tests need imported file extension for ES modules support
+        'import/extensions': ['warn', 'always', { js: 'ignorePackages' }],
+        'max-len': ['error', { code: 120 }],
+        quotes: ['error', 'single', { avoidEscape: true }],
+        semi: ['error', 'never'],
+        'no-underscore-dangle': ['error', { allowAfterThis: true }],
+        indent: ['error', 4],
+        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+        'no-multiple-empty-lines': ['error', { max: 2 }],
+        'padded-blocks': ['error', 'always', { allowSingleLineBlocks: true }],
+        'lines-between-class-members': ['error', 'always'],
+        'arrow-parens': ['error', 'as-needed', { requireForBlockBody: false }],
+        'padding-line-between-statements': [
+            'error',
+            { blankLine: 'always', prev: 'var', next: 'return' },
+            { blankLine: 'always', prev: 'block-like', next: 'return' }
+        ],
+        'prefer-template': 0, // Allow simple string concatenation
+        'comma-dangle': ['error', {
+            arrays: 'never',
+            objects: 'never',
+            imports: 'never',
+            exports: 'never',
+            functions: 'never'
+        }]
+    },
+    ignorePatterns: ['node_modules/**/*']
 }
