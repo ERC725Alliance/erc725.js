66c9ecb5dbe6ff167df2d4faf8e7cc20e96d754a
Reto Ryter <reto@twy.gmbh>
2021-07-03 04:56:51 +0000

    chore(docs): ignore .docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/rryter/Projects/erc725.js/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/rryter/Projects/erc725.js/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/rryter/Projects/erc725.js/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/rryter/Projects/erc725.js/node_modules/infima/dist/css/default/default.css"),
-  require("/home/rryter/Projects/erc725.js/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-];
