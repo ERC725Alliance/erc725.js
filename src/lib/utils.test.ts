73aa264230f10efc81f1546a8dad1f0766741f52
Hugo Masclet <hugo@lukso.io>
2021-10-13 19:08:19 +0200

    refactor: make flattenEncodedData independant
    The test does not rely anymore on external fn A to test fn B.

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,51 +1,14 @@
 import assert from 'assert';
 import { keccak256 } from 'web3-utils';
-import {
-  ERC725JSONSchema,
-  ERC725JSONSchemaKeyType,
-} from '../types/ERC725JSONSchema';
+import { ERC725JSONSchemaKeyType } from '../types/ERC725JSONSchema';
 
 import { SUPPORTED_HASH_FUNCTION_STRINGS } from './constants';
 import {
   flattenEncodedData,
-  encodeData,
   encodeKeyName,
   guessKeyTypeFromKeyName,
   isDataAuthentic,
 } from './utils';
-
-const schema: ERC725JSONSchema[] = [
-  {
-    name: 'SupportedStandards:ERC725Account',
-    key: '0xeafec4d89fa9619884b6b89135626455000000000000000000000000afdeb5d6',
-    keyType: 'Mapping',
-    valueContent: '0xafdeb5d6',
-    valueType: 'bytes',
-  },
-  {
-    name: 'LSP3Profile',
-    key: '0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5',
-    keyType: 'Singleton',
-    valueContent: 'JSONURL',
-    valueType: 'bytes',
-  },
-  {
-    name: 'LSP1UniversalReceiverDelegate',
-    key: '0x0cfc51aec37c55a4d0b1a65c6255c4bf2fbdf6277f3cc0730c45b828b6db8b47',
-    keyType: 'Singleton',
-    valueContent: 'Address',
-    valueType: 'address',
-  },
-  {
-    name: 'LSP3IssuedAssets[]',
-    key: '0x3a47ab5bd3a594c3a8995f8fa58d0876c96819ca4516bd76100c92462f2f9dc0',
-    keyType: 'Array',
-    valueContent: 'Number',
-    valueType: 'uint256',
-    elementValueContent: 'Address',
-    elementValueType: 'address',
-  },
-];
 
 describe('utils', () => {
   describe('#flattenEncodedData', () => {
@@ -67,50 +30,58 @@ describe('utils', () => {
     });
 
     it('should flatten encodedData (with multiple keys)', () => {
-      const data = {
-        LSP1UniversalReceiverDelegate:
-          '0x1183790f29BE3cDfD0A102862fEA1a4a30b3AdAb',
-        LSP3Profile: {
-          hashFunction: 'keccak256(utf8)',
-          hash: '0x820464ddfac1bec070cc14a8daf04129871d458f2ca94368aae8391311af6361',
-          url: 'ifps://QmYr1VJLwerg6pEoscdhVGugo39pa6rycEZLjtRPDfW84UAx',
+      const encodedDataManyKeys = {
+        item1: {
+          key: '0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5',
+          value:
+            '0x6f357c6a820464ddfac1bec070cc14a8daf04129871d458f2ca94368aae8391311af6361696670733a2f2f516d597231564a4c776572673670456f73636468564775676f3339706136727963455a4c6a7452504466573834554178',
         },
-        'LSP3IssuedAssets[]': [
-          '0xD94353D9B005B3c0A9Da169b768a31C57844e490',
-          '0xDaea594E385Fc724449E3118B2Db7E86dFBa1826',
-        ],
+        item2: {
+          key: '0x0cfc51aec37c55a4d0b1a65c6255c4bf2fbdf6277f3cc0730c45b828b6db8b47',
+          value: '0x1183790f29be3cdfd0a102862fea1a4a30b3adab',
+        },
+        item3: {
+          key: 'array',
+          value: [
+            {
+              key: '0x3a47ab5bd3a594c3a8995f8fa58d0876c96819ca4516bd76100c92462f2f9dc0',
+              value:
+                '0x0000000000000000000000000000000000000000000000000000000000000002',
+            },
+            {
+              key: '0x3a47ab5bd3a594c3a8995f8fa58d087600000000000000000000000000000000',
+              value: '0xd94353d9b005b3c0a9da169b768a31c57844e490',
+            },
+            {
+              key: '0x3a47ab5bd3a594c3a8995f8fa58d087600000000000000000000000000000001',
+              value: '0xdaea594e385fc724449e3118b2db7e86dfba1826',
+            },
+          ],
+        },
       };
 
-      const encodedDataManyKeys = encodeData(data, schema);
       const flattenedEncodedData = flattenEncodedData(encodedDataManyKeys);
 
       assert.deepStrictEqual(flattenedEncodedData, [
         {
-          // LSP1UniversalReceiverDelegate
-          key: '0x0cfc51aec37c55a4d0b1a65c6255c4bf2fbdf6277f3cc0730c45b828b6db8b47',
-          value: data.LSP1UniversalReceiverDelegate.toLowerCase(),
+          key: encodedDataManyKeys.item2.key,
+          value: encodedDataManyKeys.item2.value,
         },
         {
-          // LSP3IssuedAssets[0]
-          key: '0x3a47ab5bd3a594c3a8995f8fa58d087600000000000000000000000000000000',
-          value: data['LSP3IssuedAssets[]'][0].toLowerCase(),
+          key: encodedDataManyKeys.item3.value[1].key,
+          value: encodedDataManyKeys.item3.value[1].value,
         },
         {
-          // LSP3IssuedAssets[1]
-          key: '0x3a47ab5bd3a594c3a8995f8fa58d087600000000000000000000000000000001',
-          value: data['LSP3IssuedAssets[]'][1].toLowerCase(),
+          key: encodedDataManyKeys.item3.value[2].key,
+          value: encodedDataManyKeys.item3.value[2].value,
         },
         {
-          // LSP3IssuedAssets[]
-          key: '0x3a47ab5bd3a594c3a8995f8fa58d0876c96819ca4516bd76100c92462f2f9dc0',
-          value:
-            '0x0000000000000000000000000000000000000000000000000000000000000002',
+          key: encodedDataManyKeys.item3.value[0].key,
+          value: encodedDataManyKeys.item3.value[0].value,
         },
         {
-          // LSP3Profile
-          key: '0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5',
-          value:
-            '0x6f357c6a820464ddfac1bec070cc14a8daf04129871d458f2ca94368aae8391311af6361696670733a2f2f516d597231564a4c776572673670456f73636468564775676f3339706136727963455a4c6a7452504466573834554178',
+          key: encodedDataManyKeys.item1.key,
+          value: encodedDataManyKeys.item1.value,
         },
       ]);
     });
