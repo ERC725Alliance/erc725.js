4f849dac01116e6f019e04fea950b42d2271910b
Hugo Masclet <git@hugom.xyz>
2022-02-11 11:16:06 +0100

    feat: add LSP1 schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,11 +1,13 @@
 import { ERC725JSONSchema } from '../types/ERC725JSONSchema';
 
+import LSP1UniversalReceiverDelegate from './LSP1UniversalReceiverDelegate.json';
 import LSP3UniversalProfile from './LSP3UniversalProfile.json';
 import LSP4DigitalAsset from './LSP4DigitalAsset.json';
 import LSP5ReceivedAssets from './LSP5ReceivedAssets.json';
 import LSP6KeyManager from './LSP6KeyManager.json';
 
-export default LSP3UniversalProfile.concat(
+export default LSP1UniversalReceiverDelegate.concat(
+  LSP3UniversalProfile,
   LSP4DigitalAsset,
   LSP5ReceivedAssets,
   LSP6KeyManager,
