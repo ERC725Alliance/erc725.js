908aef0a3477dfafad3b862ebd8dd334bfddc6d8
Hugo Masclet <hugo@lukso.io>
2022-04-04 18:45:25 +0200

    feat: add schemas at the root and improve docs

--- docs/schemas.md
+++ docs/schemas.md
@@ -20,3 +20,15 @@ documentation](https://github.com/lukso-network/LIPs/blob/master/LSPs/LSP-2-ERC7
 - `keyType`: One of the supported erc725 keyTypes
 - `valueContent`: The described content type for parsing
 - `valueType`: The type of the content data in store for decoding
+
+## LSP Schemas
+
+The most common schemas from LUKSO [LSPs](https://github.com/lukso-network/LIPs/tree/main/LSPs) are available in the [`schemas/`](https://github.com/ERC725Alliance/erc725.js/tree/develop/schemas) folder.
+
+You can import them from:
+
+```js
+import LSP3 from '@erc725/erc725.js/schemas/LSP3UniversalProfile.json';
+import LSP5 from '@erc725/erc725.js/schemas/LSP5ReceivedAssets.json';
+// ...
+```
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,11 +1,11 @@
 import { ERC725JSONSchema } from '../types/ERC725JSONSchema';
 
-import LSP1UniversalReceiverDelegate from './LSP1UniversalReceiverDelegate.json';
-import LSP3UniversalProfile from './LSP3UniversalProfile.json';
-import LSP4DigitalAssetLegacy from './LSP4DigitalAssetLegacy.json';
-import LSP4DigitalAsset from './LSP4DigitalAsset.json';
-import LSP5ReceivedAssets from './LSP5ReceivedAssets.json';
-import LSP6KeyManager from './LSP6KeyManager.json';
+import LSP1UniversalReceiverDelegate from '../../schemas/LSP1UniversalReceiverDelegate.json';
+import LSP3UniversalProfile from '../../schemas/LSP3UniversalProfileMetadata.json';
+import LSP4DigitalAssetLegacy from '../../schemas/LSP4DigitalAssetLegacy.json';
+import LSP4DigitalAsset from '../../schemas/LSP4DigitalAsset.json';
+import LSP5ReceivedAssets from '../../schemas/LSP5ReceivedAssets.json';
+import LSP6KeyManager from '../../schemas/LSP6KeyManager.json';
 
 export default LSP1UniversalReceiverDelegate.concat(
   LSP3UniversalProfile,
