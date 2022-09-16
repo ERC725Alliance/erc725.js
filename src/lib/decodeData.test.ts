92d0090dc41f9b007f4241636a2491ee78da23bc
b00ste.lyx <62855857+b00ste@users.noreply.github.com>
2022-09-16 19:28:54 +0300

    Update src/lib/decodeData.test.ts
    Co-authored-by: Hugo Masclet <git@hugom.xyz>

--- src/lib/decodeData.test.ts
+++ src/lib/decodeData.test.ts
@@ -183,8 +183,8 @@ describe('tuple', () => {
       {
         valueContent: '(Bytes4,Number)',
         valueType: '(bytes4,bytes8)',
-        encodedValue: '0x5fcaac27000000000000000c',
-        decodedValue: ['0x5fcaac27', '12'],
+        encodedValue: '0xdeadbeaf000000000000000c',
+        decodedValue: ['0xdeadbeaf', '12'],
       },
     ]; // TODO: add more cases? Address, etc.
 
