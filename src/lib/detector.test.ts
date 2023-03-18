6ee932e66e0cb543d6ccc3d3ca3c3600f916adca
YamenMerhi <yamennmerhi@gmail.com>
2023-03-18 17:42:44 +0200

    test: test edge cases

--- src/lib/detector.test.ts
+++ src/lib/detector.test.ts
@@ -64,8 +64,40 @@ describe('supportsInterface', () => {
   });
 });
 
-describe('checkPermissions', () => {
+describe.only('checkPermissions', () => {
   describe('test with single permission', () => {
+    it('should throw an error when given an invalid permission string', async () => {
+      const requiredPermissions = 'INVALIDPERMISSION';
+      const grantedPermissions =
+        '0x000000000000000000000000000000000000000000000000000000000000ff51';
+      expect(() =>
+        checkPermissions(requiredPermissions, grantedPermissions),
+      ).to.throw(
+        'Invalid permission string. It must be a valid 32-byte hex string or a known permission name.',
+      );
+    });
+
+    it('should throw an error when given an invalid 32-byte hex string', async () => {
+      const requiredPermissions = '0xinvalidhexstring';
+      const grantedPermissions =
+        '0x000000000000000000000000000000000000000000000000000000000000ff51';
+      expect(() =>
+        checkPermissions(requiredPermissions, grantedPermissions),
+      ).to.throw(
+        'Invalid permission string. It must be a valid 32-byte hex string or a known permission name.',
+      );
+    });
+
+    it('should throw an error when given an invalid grantedPermission 32-byte hex string', async () => {
+      const requiredPermissions = 'CHANGEOWNER';
+      const grantedPermissions = '0xinvalidgrantedpermissionhexstring';
+      expect(() =>
+        checkPermissions(requiredPermissions, grantedPermissions),
+      ).to.throw(
+        'Invalid grantedPermissions string. It must be a valid 32-byte hex string.',
+      );
+    });
+
     it('should return true when single literal permission matches granted permissions', async () => {
       const requiredPermissions = 'CHANGEOWNER';
       const grantedPermissions =
@@ -102,6 +134,28 @@ describe('checkPermissions', () => {
   });
 
   describe('test with multiple permissions', () => {
+    it('should throw an error when given an array containing an invalid permission string', async () => {
+      const requiredPermissions = ['CHANGEOWNER', 'INVALIDPERMISSION'];
+      const grantedPermissions =
+        '0x000000000000000000000000000000000000000000000000000000000000ff51';
+      expect(() =>
+        checkPermissions(requiredPermissions, grantedPermissions),
+      ).to.throw(
+        'Invalid permission string. It must be a valid 32-byte hex string or a known permission name.',
+      );
+    });
+
+    it('should throw an error when given an array containing an invalid 32-byte hex string', async () => {
+      const requiredPermissions = ['CHANGEOWNER', '0xinvalidhexstring'];
+      const grantedPermissions =
+        '0x000000000000000000000000000000000000000000000000000000000000ff51';
+      expect(() =>
+        checkPermissions(requiredPermissions, grantedPermissions),
+      ).to.throw(
+        'Invalid permission string. It must be a valid 32-byte hex string or a known permission name.',
+      );
+    });
+
     it('should return false when one of the literal permissions does not match granted permissions', async () => {
       const requiredPermissions = ['CHANGEPERMISSIONS', 'CALL'];
       const grantedPermissions =
