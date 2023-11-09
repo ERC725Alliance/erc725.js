a9a24393f6e84841aeb260b926a9532917f89c4a
CJ42 <cavallerajean@gmail.com>
2023-11-09 19:28:49 +0000

    refactor: improve method to extract tuple elements

--- src/lib/decodeData.ts
+++ src/lib/decodeData.ts
@@ -50,17 +50,8 @@ const isValidTupleDefinition = (tupleContent: string): boolean => {
   return true;
 };
 
-const extractValueTypesFromTuple = (tupleContent: string): string[] => {
-  let valueTypeToDecode = tupleContent;
-
-  if (tupleContent.includes(COMPACT_BYTES_ARRAY_STRING)) {
-    valueTypeToDecode = tupleContent.replace(COMPACT_BYTES_ARRAY_STRING, '');
-  }
-
-  return valueTypeToDecode
-    .substring(1, valueTypeToDecode.length - 1)
-    .split(',');
-};
+const extractTupleElements = (tupleContent: string): string[] =>
+  tupleContent.substring(1, tupleContent.length - 1).split(',');
 
 const extractTupleElements = (tupleContent: string): string[] =>
   tupleContent.substring(1, tupleContent.length - 1).split(',');
@@ -148,11 +139,11 @@ export const decodeTupleKeyValue = (
 ): Array<string> => {
   // We assume data has already been validated at this stage
 
-  const valueTypeParts = extractValueTypesFromTuple(valueType);
+  // Sanitize the string to keep only the tuple, if we are dealing with `CompactBytesArray`
+  const valueTypeToDecode = valueType.replace(COMPACT_BYTES_ARRAY_STRING, '');
 
-  const valueContentParts = valueContent
-    .substring(1, valueContent.length - 1)
-    .split(',');
+  const valueTypeParts = extractTupleElements(valueTypeToDecode);
+  const valueContentParts = extractTupleElements(valueContent);
 
   const bytesLengths: number[] = [];
 
