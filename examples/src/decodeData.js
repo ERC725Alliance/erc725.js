b3a273af3f19951f9f2831230251c7e78fc389df
Hugo Masclet <hugo@lukso.io>
2023-12-14 13:23:12 +0100

    docs: update README and examples and docs for VerifiableURI

--- examples/src/decodeData.js
+++ examples/src/decodeData.js
@@ -27,29 +27,6 @@ const decodedDataOneKey = myERC725.decodeData([
 ]
 */
 
-const decodedDataOneKeyOld = myERC725.decodeData([
-  {
-    keyName: 'LSP3Profile',
-    value:
-      '0x6f357c6a820464ddfac1bec070cc14a8daf04129871d458f2ca94368aae8391311af6361697066733a2f2f516d597231564a4c776572673670456f73636468564775676f3339706136727963455a4c6a7452504466573834554178',
-  },
-]);
-/**
-[
-  {
-    name: 'LSP3Profile',
-    key: '0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5',
-    value: {
-      verification: {
-        method: 'keccak256(utf8)',
-        data: '0x820464ddfac1bec070cc14a8daf04129871d458f2ca94368aae8391311af6361',
-      },
-      url: 'ipfs://QmYr1VJLwerg6pEoscdhVGugo39pa6rycEZLjtRPDfW84UAx'
-    }
-  }
-]
-*/
-
 const decodedDataManyKeys = myERC725.decodeData([
   {
     keyName: 'LSP3Profile',
@@ -102,10 +79,6 @@ console.log('/*--------------------------------------------/*');
 console.log('/* decodeData - one key (VerifiableURI)       /*');
 console.log('/*--------------------------------------------/*');
 console.log(decodedDataOneKey);
-console.log('/*--------------------------------------------/*');
-console.log('/* decodeData - one key (JSONURL)             /*');
-console.log('/*--------------------------------------------/*');
-console.log(decodedDataOneKeyOld);
 console.log('/*--------------------------------------------/*');
 console.log('/* decodeData - many keys                     /*');
 console.log('/*--------------------------------------------/*');
