c54a370c1e4f8d71ba6bb7e62122b89069f521e6
CJ42 <cavallerajean@gmail.com>
2024-01-22 16:44:40 +0000

    feat: allow to load typed schemas from `schemas/` folder

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,26 +1,27 @@
 import { ERC725JSONSchema } from '../types/ERC725JSONSchema';
 
-import LSP1UniversalReceiverDelegate from '../../schemas/LSP1UniversalReceiverDelegate.json';
-import LSP3Profile from '../../schemas/LSP3ProfileMetadata.json';
-import LSP4DigitalAssetLegacy from '../../schemas/LSP4DigitalAssetLegacy.json';
-import LSP4DigitalAsset from '../../schemas/LSP4DigitalAsset.json';
-import LSP5ReceivedAssets from '../../schemas/LSP5ReceivedAssets.json';
-import LSP6KeyManager from '../../schemas/LSP6KeyManager.json';
-import LSP8IdentifiableDigitalAsset from '../../schemas/LSP8IdentifiableDigitalAsset.json';
-import LSP9Vault from '../../schemas/LSP9Vault.json';
-import LSP10ReceivedVaults from '../../schemas/LSP10ReceivedVaults.json';
-import LSP12IssuedAssets from '../../schemas/LSP12IssuedAssets.json';
-import LSP17ContractExtension from '../../schemas/LSP17ContractExtension.json';
+import LSP1JSONSchema from '../../schemas/LSP1UniversalReceiverDelegate.json';
+import LSP3JSONSchema from '../../schemas/LSP3ProfileMetadata.json';
+import LSP4JSONSchema from '../../schemas/LSP4DigitalAsset.json';
+import LSP4LegacyJSONSchema from '../../schemas/LSP4DigitalAssetLegacy.json';
+import LSP5JSONSchema from '../../schemas/LSP5ReceivedAssets.json';
+import LSP6JSONSchema from '../../schemas/LSP6KeyManager.json';
+import LSP8JSONSchema from '../../schemas/LSP8IdentifiableDigitalAsset.json';
+import LSP9JSONSchema from '../../schemas/LSP9Vault.json';
+import LSP10JSONSchema from '../../schemas/LSP10ReceivedVaults.json';
+import LSP12JSONSchema from '../../schemas/LSP12IssuedAssets.json';
+import LSP17JSONSchema from '../../schemas/LSP17ContractExtension.json';
 
-export default LSP1UniversalReceiverDelegate.concat(
-  LSP3Profile,
-  LSP4DigitalAssetLegacy,
-  LSP4DigitalAsset,
-  LSP5ReceivedAssets,
-  LSP6KeyManager,
-  LSP8IdentifiableDigitalAsset,
-  LSP9Vault,
-  LSP10ReceivedVaults,
-  LSP12IssuedAssets,
-  LSP17ContractExtension,
-) as ERC725JSONSchema[];
+type schemaType = ERC725JSONSchema[];
+
+export const LSP1Schema: schemaType = LSP1JSONSchema as schemaType;
+export const LSP3Schema: schemaType = LSP3JSONSchema as schemaType;
+export const LSP4Schema: schemaType = LSP4JSONSchema as schemaType;
+export const LSP4LegacySchema: schemaType = LSP4LegacyJSONSchema as schemaType;
+export const LSP5Schema: schemaType = LSP5JSONSchema as schemaType;
+export const LSP6Schema: schemaType = LSP6JSONSchema as schemaType;
+export const LSP8Schema: schemaType = LSP8JSONSchema as schemaType;
+export const LSP9Schema: schemaType = LSP9JSONSchema as schemaType;
+export const LSP10Schema: schemaType = LSP10JSONSchema as schemaType;
+export const LSP12Schema: schemaType = LSP12JSONSchema as schemaType;
+export const LSP17Schema: schemaType = LSP17JSONSchema as schemaType;
--- src/types/ERC725JSONSchema.ts
+++ src/types/ERC725JSONSchema.ts
@@ -159,7 +159,7 @@ export function isValidValueType(
 export interface ERC725JSONSchema {
   name: string; // Describes the name of the key, SHOULD compromise of the Standards name + sub type. e.g: LSP2Name
   key: string; // The keccak256 hash of the name. This is the actual key that MUST be retrievable via ERC725Y.getData(bytes32 key)
-  keyType: ERC725JSONSchemaKeyType; // Types that determine how the values should be interpreted.
+  keyType: ERC725JSONSchemaKeyType | string; // Types that determine how the values should be interpreted.
   valueContent: ERC725JSONSchemaValueContent | string; // string holds '0x1345ABCD...' If the value content are specific bytes, than the returned value is expected to equal those bytes.
   valueType: ERC725JSONSchemaValueType | string; // The type of the value. This is used to determine how the value should be encoded / decode (`string` for tuples and CompactBytesArray).
 }
