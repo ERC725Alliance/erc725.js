27da039ca7055875e7bea5b129987f1da5b90376
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-09-17 10:11:52 -0400

    fix: Repair patchIPFSUtlsIfApplication to only replace if the URL starts with ipfs://

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -613,7 +613,9 @@ export function patchIPFSUrlsIfApplicable(
   receivedData: URLDataWithHash,
   ipfsGateway: string,
 ): URLDataWithHash {
-  if (receivedData?.url?.indexOf('ipfs://') !== -1) {
+  // Only map URL if it's indeed an ipfs:// URL and ignore if it's a data:// URL with JSON
+  // possibly containing an IPFS URL inside of the JSON data.
+  if (receivedData?.url?.startsWith('ipfs://')) {
     return {
       ...receivedData,
       url: receivedData.url.replace('ipfs://', ipfsGateway),
